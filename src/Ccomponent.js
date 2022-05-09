085fa987b025f26b68d69b491241f234be9f2fbc
tetiana.korobkina <tetiana.korobkina@chyron.com>
2022-05-09 16:45:32 +0200

    add counter (increment, decrement, state)

--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -3,38 +3,43 @@ import { Menu } from './Menu'
 
 export default class Ccomponent extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   
     this.state = {
-       visibility: false
-    }
+       count: 0
+    };
 
-    this.handelClick = this.handelClick.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
-  handelClick() {
+  increment() {
     this.setState(state => ({
-      visibility: !state.visibility
+      count: state.count + 1
     }));
   }
 
+  decrement() {
+    this.setState(state => ({
+      count: state.count - 1
+    }));
+  }
+
+  reset() {
+    this.setState({
+      count: 0
+    })
+  }
+
   render() {
-    if (this.state.visibility){
-    return (
+    return(
       <div>
-        <h1>
-            Now you see me
-        </h1>
-        <button onClick={this.handelClick}>Click</button>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
+        <button onClick={this.reset}>reset</button>
+        <h1>Current: {this.state.count}</h1>
       </div>
     )
   }
-  else {
-    return (
-      <div>
-        <button onClick={this.handelClick}>Click</button>
-      </div>
-    )
-  }
-}
 }
