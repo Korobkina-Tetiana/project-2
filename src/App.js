0091518874bdec7258801cfb8d8ea36203c698b4
tetiana.korobkina <tetiana.korobkina@chyron.com>
2022-05-09 16:04:21 +0200

    add state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Fcomponent name="Chyron" />
         <Ccomponent />
   
         <img src={logo} className="App-logo" alt="logo" />
--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react'
 import { Menu } from './Menu'
 
 export default class Ccomponent extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       name: "David"
+    }
+  }
+  
   render() {
+    const name = this.state.name;
     return (
       <div>
-        <Menu />
         <h1>
-            Class component {this.props.name}
+            Class component {name}
         </h1>
       </div>
     )
