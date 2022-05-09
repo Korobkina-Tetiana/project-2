fc3a337372565f91d6de8ce4c47878954ce7f4a4
tetiana.korobkina <tetiana.korobkina@chyron.com>
2022-05-09 14:42:44 +0200

    creating a functional component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Fcomponent from './Fcomponent';
 import Ccomponent from './Ccomponent';
-import { Menu } from './Menu';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Fcomponent />
+        <Fcomponent name="Chyron" />
         <Ccomponent />
   
         <img src={logo} className="App-logo" alt="logo" />
--- src/Fcomponent.js
+++ src/Fcomponent.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Fcomponent() {
+export default function Fcomponent(props) {
   return (
     <div>
         <h1>
-            Func component
+            Hello, {props.name}
         </h1>
     </div>
   )
