c4161ab91f6001bca05a0d5cd5d85379b151b8a0
tetiana.korobkina <tetiana.korobkina@chyron.com>
2022-05-09 10:07:51 +0200

    connected the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Fcomponent from './Fcomponent';
+import Ccomponent from './Ccomponent';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Fcomponent />
+        <Ccomponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         Web Developer Blog
